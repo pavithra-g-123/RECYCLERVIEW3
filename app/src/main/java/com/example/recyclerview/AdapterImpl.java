c55cbc55d2588ff3d43ea9b05f984750e5ae5fed
pavithra <pavithrag092002@gmail.com>
2021-04-25 12:34:27 +0530

    Blood bank

---
+++ app/src/main/java/com/example/recyclerview/AdapterImpl.java
@@ -0,0 +1,9 @@
+package com.example.recyclerview;
+
+import java.util.List;
+
+public class AdapterImpl extends Adapter {
+    public AdapterImpl(List<ModelClass> modelClassList) {
+        super(modelClassList);
+    }
+}
